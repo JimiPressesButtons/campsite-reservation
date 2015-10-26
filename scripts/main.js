f770d131d96cd41b8135d494e0aa3bfc914c0536
James <james.stitts@gmail.com>
2015-10-26 10:54:32 -0500

    added routeter, navcomponent, boiler plates

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,17 @@
 'use strict';
+var React = require('react');
+var ReactDOM = require('react-dom');
+var Backbone = require('backbone');
+var NavComponent = require('./components/NavComponent');
+
+var Router = new Backbone.Router.extend({
+
+
+});
+
+var r = new Router();
+Backbone.history.start();
+
+ReactDom.render(
+	<NavComponent />, document.getElementById('nav')
+);
